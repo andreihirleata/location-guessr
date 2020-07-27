6a9d251752171ac2fa49634b3f315981e57e6f3e
Shelby <shelby.harrison6@hotmail.com>
2020-07-27 20:03:55 +0100

    Test commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import React from "react";
 import "../styles/App.css";
 
 function App() {
-  return <div className="App">Hello There</div>;
-}
+  return <div className="App">Hello There
+  <h2>Location Gussr</h2>
+  </div>
+};
 
 export default App;
