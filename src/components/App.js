e16b1f8a623756febf5426a747a9ade6a9799cfd
Andrei Hirleata <hirleata.andrei@gmail.com>
2020-08-01 14:29:27 +0100

    added random streetvi ew component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-
 import "../styles/App.css";
+import RandomStreetview from "./RandomStreetview";
 
 function App() {
-  return <div className="App">Hello There</div>;
+  return <div>{/* <RandomStreetview></RandomStreetview> */}</div>;
 }
 
 export default App;
---
+++ src/components/RandomStreetview.js
@@ -0,0 +1,43 @@
+import randomStreetView from "random-streetview";
+import React, { useState, useEffect } from "react";
+import ReactStreetview from "react-streetview";
+
+const RandomStreetview = () => {
+  const googleMapsApiKey = "AIzaSyCdtPEreWplsxM-Ir6nnyNOgrTJSZURJO4";
+
+  const [coord, setCoord] = useState("");
+
+  useEffect(() => {
+    const getCoords = async () => {
+      const data = await randomStreetView.getRandomLocation();
+      setCoord({
+        position: { lat: data[0], lng: data[1] },
+        pov: { heading: 100, pitch: 0 },
+        addressControl: false,
+        showRoadLabels: false,
+        zoomControl: false,
+        panControl: false,
+      });
+    };
+    getCoords();
+  }, []);
+
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "#eeeeee",
+      }}
+    >
+      {coord && (
+        <ReactStreetview
+          apiKey={googleMapsApiKey}
+          streetViewPanoramaOptions={coord}
+        />
+      )}
+    </div>
+  );
+};
+
+export default RandomStreetview;
