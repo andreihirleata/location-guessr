b90f7ee7d37795ca8a4ba91dc1fe34ee243cab82
Andrei Hirleata <hirleata.andrei@gmail.com>
2020-08-01 15:57:55 +0100

    revert

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,14 @@ import "../styles/App.css";
 import RandomStreetview from "./RandomStreetview";
 
 function App() {
-  return (
-    <div>
-      <RandomStreetview />
-    </div>
-  );
+<<<<<<< HEAD
+  return <div>{/* <RandomStreetview></RandomStreetview> */}</div>;
 }
+=======
+  return <div className="App">Hello There
+  <h2>Location Gussr</h2>
+  </div>
+};
+>>>>>>> d29192d6bb870c497ee269b9a8685c9a35e7cb74
+
 export default App;
