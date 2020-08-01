7d1d81293b1955ddbcb463da1763dd95af48e008
Andrei Hirleata <hirleata.andrei@gmail.com>
2020-08-01 16:04:05 +0100

    added styling, home/gamescreen, routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,16 @@
 import React from "react";
-import HomeScreen from './HomeScreen';
+import HomeScreen from "./HomeScreen";
 import "../styles/App.css";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 import GameScreen from "./GameScreen";
 
 function App() {
   return (
     <Switch>
-    <Route exact path="/" component={HomeScreen}
-    />
-    <Route exact path="/game" component={GameScreen}
-    />
+      <Route exact path="/" component={HomeScreen} />
+      <Route exact path="/game" component={GameScreen} />
     </Switch>
-  )
-};
+  );
+}
 
 export default App;
